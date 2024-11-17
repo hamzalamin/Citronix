f45e709103d3a2e8578f3e8d3d541e6db0befa15
hamzalamin <hamzalamin80@gmail.com>
2024-11-17 18:29:12 +0100

    feat: add CreateFarmDto for farm creation requests
    - Introduced CreateFarmDto to handle data for creating new farms.

---
+++ src/main/java/com/wora/citronix/models/DTOs/farmDtos/CreateFarmDto.java
@@ -0,0 +1,15 @@
+package com.wora.citronix.models.DTOs.farmDtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.time.LocalDate;
+
+public record CreateFarmDto(
+        @NotBlank String name,
+        @NotBlank String localization,
+        @NotNull @Positive Double surface,
+        @NotNull LocalDate creationDate
+) {
+}
