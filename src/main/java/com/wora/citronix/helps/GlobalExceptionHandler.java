4bbff6922e3b4220a49820e63e3404fc07134e71
hamzalamin <hamzalamin80@gmail.com>
2024-11-17 23:15:41 +0100

    feat: add global exception handler for custom and validation errors
    - Implemented GlobalExceptionHandler to handle EntityNotFoundException and validation errors.
    - Returns appropriate HTTP status and messages for exceptions across the application.

---
+++ src/main/java/com/wora/citronix/helps/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.wora.citronix.helps;
+
+import com.wora.citronix.exceptions.EntityNotFoundException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
