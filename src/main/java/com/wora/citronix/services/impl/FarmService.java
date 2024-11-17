ad5e7517696a5719e9697a3e333be2d23961f761
hamzalamin <hamzalamin80@gmail.com>
2024-11-17 21:50:30 +0100

    CIT-22 feat: implement save method in FarmService
    - Added logic to map CreateFarmDto to Farm entity, save it, and return FarmDto.

--- src/main/java/com/wora/citronix/services/impl/FarmService.java
+++ src/main/java/com/wora/citronix/services/impl/FarmService.java
@@ -1,16 +1,29 @@
 package com.wora.citronix.services.impl;
 
+import com.wora.citronix.mappers.FarmMapper;
 import com.wora.citronix.models.DTOs.farmDtos.CreateFarmDto;
 import com.wora.citronix.models.DTOs.farmDtos.FarmDto;
 import com.wora.citronix.models.DTOs.farmDtos.UpdateFarmDto;
+import com.wora.citronix.models.entities.Farm;
+import com.wora.citronix.repositories.FarmRepository;
 import com.wora.citronix.services.inter.IFarmService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class FarmService implements IFarmService {
+
+    private final FarmRepository farmRepository;
+    private final FarmMapper farmMapper;
+
     @Override
     public FarmDto save(CreateFarmDto createFarmDto) {
-        return null;
+        Farm farm = farmMapper.toEntity(createFarmDto);
+        Farm savedFarm = farmRepository.save(farm);
+        return farmMapper.toDto(savedFarm);
     }
 
     @Override
