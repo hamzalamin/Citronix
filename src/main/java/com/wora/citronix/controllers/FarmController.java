d35da3fc8e94786f61a839aa327f2800d3afec42
hamzalamin <hamzalamin80@gmail.com>
2024-11-18 00:15:23 +0100

    feat: add endpoint to fetch all farms with pagination
    - Added GET /api/v1/farms endpoint to retrieve a paginated list of farms.
    - Used pageNumber and size query parameters to fetch the corresponding page of farms.

--- src/main/java/com/wora/citronix/controllers/FarmController.java
+++ src/main/java/com/wora/citronix/controllers/FarmController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -21,6 +20,15 @@ public class FarmController {
     @PostMapping("/farms")
     public ResponseEntity<FarmDto> create(@RequestBody @Valid CreateFarmDto farmDto){
         return new ResponseEntity<>(farmService.save(farmDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/farms")
+    public ResponseEntity<List<FarmDto>> findAll(
+            @RequestParam Integer pageNumber,
+            @RequestParam Integer size
+    ){
+        List<FarmDto> farms = farmService.findAll(pageNumber, size);
+        return new ResponseEntity<>(farms, HttpStatus.OK);
     }
 
 }
