169165984a8cd1c686a4e0aef6c12c01a4140758
hamzalamin <hamzalamin80@gmail.com>
2024-11-17 18:32:23 +0100

    feat: add EmbeddedFarmDto for farm data representation
    - Introduced EmbeddedFarmDto for lightweight farm data representation.

---
+++ src/main/java/com/wora/citronix/models/DTOs/farmDtos/EmbeddedFarmDto.java
@@ -0,0 +1,16 @@
+package com.wora.citronix.models.DTOs.farmDtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.time.LocalDate;
+
+public record EmbeddedFarmDto(
+        @NotNull @Positive Long id,
+        @NotBlank String name,
+        @NotBlank String localization,
+        @NotNull @Positive Double surface,
+        @NotNull LocalDate creationDate
+) {
+}
