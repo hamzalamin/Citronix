e35e3337d269aa0db44bea78efa5559335c7c974
hamzalamin <hamzalamin80@gmail.com>
2024-11-18 13:11:08 +0100

    feat: update FieldMapper to support CreateFieldDto and UpdateFieldDto
    - Added methods to FieldMapper for mapping CreateFieldDto and UpdateFieldDto to Field entity.
    - Enhanced the mapper interface to handle all required DTO-to-entity conversions.

--- src/main/java/com/wora/citronix/mappers/FieldMapper.java
+++ src/main/java/com/wora/citronix/mappers/FieldMapper.java
@@ -1,10 +1,14 @@
 package com.wora.citronix.mappers;
 
 import com.wora.citronix.mappers.api.GenericMapper;
+import com.wora.citronix.models.DTOs.fieldDtos.CreateFieldDto;
 import com.wora.citronix.models.DTOs.fieldDtos.FieldDto;
+import com.wora.citronix.models.DTOs.fieldDtos.UpdateFieldDto;
 import com.wora.citronix.models.entities.Field;
 
 public interface FieldMapper extends GenericMapper<Field, FieldDto> {
     Field toEntity(FieldDto fieldDto);
+    Field toEntity(CreateFieldDto createFieldDto);
+    Field toEntity(UpdateFieldDto updateFieldDto);
     FieldDto toDto(Field field);
 }
