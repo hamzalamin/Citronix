c03a9f192e160ce42813e92dde5159b39d3098c6
hamzalamin <hamzalamin80@gmail.com>
2024-11-18 12:53:30 +0100

    Add EmbeddedFieldsDto with validation constraints

--- src/main/java/com/wora/citronix/models/DTOs/fieldsDtos/EmbeddedFieldsDto.java
+++ src/main/java/com/wora/citronix/models/DTOs/fieldsDtos/EmbeddedFieldsDto.java
@@ -1,6 +1,12 @@
 package com.wora.citronix.models.DTOs.fieldsDtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record EmbeddedFieldsDto(
-        Long id
+        @NotNull @Positive Long id,
+        @NotBlank String name,
+        @NotNull @Positive Double surface
 ) {
 }
