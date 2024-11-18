c631da1b8e6f2334e23cc341b7f3079c21d26f37
hamzalamin <hamzalamin80@gmail.com>
2024-11-18 12:57:56 +0100

    feat: add FieldDto with validation
    - Created the FieldDto class with necessary validation annotations.
    - Added fields for id,
    ame, surface, and an embedded arm DTO.
    - Ensured that id and surface are positive, and
    ame is not blank.

--- src/main/java/com/wora/citronix/models/DTOs/fieldsDtos/FieldDto.java
+++ src/main/java/com/wora/citronix/models/DTOs/fieldsDtos/FieldDto.java
@@ -1,7 +1,14 @@
 package com.wora.citronix.models.DTOs.fieldsDtos;
 
-public record FieldDto(
-        Long id
+import com.wora.citronix.models.DTOs.farmDtos.EmbeddedFarmDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
+public record FieldDto(
+        @NotNull @Positive Long id,
+        @NotBlank String name,
+        @NotNull @Positive Double surface,
+        EmbeddedFarmDto farm
 ) {
 }
