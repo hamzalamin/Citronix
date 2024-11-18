cc75e93a9f7cc27f93026e00d4c46571dad7aa0c
hamzalamin <hamzalamin80@gmail.com>
2024-11-18 12:51:59 +0100

    Complete Field entity with attributes, validations, and farm relationship

--- src/main/java/com/wora/citronix/models/entities/Field.java
+++ src/main/java/com/wora/citronix/models/entities/Field.java
@@ -1,6 +1,9 @@
 package com.wora.citronix.models.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +18,15 @@ public class Field {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "name", unique = true)
+    @NotBlank
+    private String name;
+
+    @Column(name = "surface")
+    @NotNull
+    @Positive
+    private Double surface;
 
     @ManyToOne
     @JoinColumn(name = "farm_id")
