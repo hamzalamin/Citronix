8de659df09e8fc431aec42cf4fd1e7a89b785155
hamzalamin <hamzalamin80@gmail.com>
2024-11-18 16:13:17 +0100

    feat: implement update method for updating field details
    - Added method to update field details, including name and surface area.
    - Validates farm surface and field number constraints before updating.
    - Throws InsufficientFarmSurfaceException if constraints are violated.
    - Updates the field and returns the updated FieldDto.

--- src/main/java/com/wora/citronix/services/impl/FieldService.java
+++ src/main/java/com/wora/citronix/services/impl/FieldService.java
@@ -27,7 +27,7 @@ public class FieldService implements IFieldService {
     @Override
     public FieldDto save(CreateFieldDto createFieldDto) {
         Field field = fieldMapper.toEntity(createFieldDto);
-        Farm farm = farmService.getFarmEntityById(createFieldDto.farmId());
+        Farm farm = getFarmById(createFieldDto.farmId());
 
         Double totalFieldsSurface = calculateFieldsSurface(farm);
         Double halfFarmSurface = farm.getSurface() / 2;
@@ -56,7 +56,30 @@ public class FieldService implements IFieldService {
 
     @Override
     public FieldDto update(UpdateFieldDto updateFieldDto, Long id) {
-        return null;
+        Field field = fieldRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Field", id));
+
+        Farm farm = getFarmById(updateFieldDto.farmId());
+
+        Double totalFieldsSurface = calculateFieldsSurface(farm);
+        Double halfFarmSurface = farm.getSurface() / 2;
+        Double farmSurface = farm.getSurface();
+        Double fieldSurface = field.getSurface();
+
+        if (calculateFieldsNumber(farm) >= 10) {
+            throw new InsufficientFarmSurfaceException("The maximum number of fields allowed on a farm is 10");
+        }
+        if (fieldSurface > halfFarmSurface) {
+            throw new InsufficientFarmSurfaceException("Field must be under 50% of the farm surface");
+        }
+        if (farmSurface <= totalFieldsSurface + fieldSurface) {
+            throw new InsufficientFarmSurfaceException("Farm surface area is insufficient for the new field");
+        }
+
+        field.setName(updateFieldDto.name());
+        field.setSurface(updateFieldDto.surface());
+        Field savedField = fieldRepository.save(field);
+        return fieldMapper.toDto(savedField);
     }
 
     @Override
@@ -78,5 +101,9 @@ public class FieldService implements IFieldService {
 
     private Integer calculateFieldsNumber(Farm farm) {
         return fieldRepository.findByFarm(farm).size();
+    }
+
+    private Farm getFarmById(Long farmId) {
+        return farmService.getFarmEntityById(farmId);
     }
 }
