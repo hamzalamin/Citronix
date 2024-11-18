9619e44b3d1cda4559818bcd8be8561bfe0cd154
hamzalamin <hamzalamin80@gmail.com>
2024-11-19 00:15:44 +0100

    feat: add planting date to Tree entity
    - Added plantingDate field to the Tree entity with @NotNull validation to store the planting date of trees.

--- src/main/java/com/wora/citronix/models/entities/Tree.java
+++ src/main/java/com/wora/citronix/models/entities/Tree.java
@@ -1,10 +1,13 @@
 package com.wora.citronix.models.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Entity(name = "trees")
 @Getter
@@ -15,6 +18,10 @@ public class Tree {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "planting_date")
+    @NotNull
+    private LocalDate plantingDate;
 
     @ManyToOne
     @JoinColumn(name = "field_id")
