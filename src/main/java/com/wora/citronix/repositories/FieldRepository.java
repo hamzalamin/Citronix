9e3d048fa6a69cb091799511591c116d871c6987
hamzalamin <hamzalamin80@gmail.com>
2024-11-19 01:17:21 +0100

    feat: add TreeRepository for managing Tree entities
    - Created TreeRepository extending JpaRepository for basic CRUD operations on Tree entities.

--- src/main/java/com/wora/citronix/repositories/FieldRepository.java
+++ src/main/java/com/wora/citronix/repositories/FieldRepository.java
@@ -3,9 +3,11 @@ package com.wora.citronix.repositories;
 import com.wora.citronix.models.entities.Farm;
 import com.wora.citronix.models.entities.Field;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface FieldRepository extends JpaRepository<Field, Long> {
     List<Field> findByFarm(Farm farm);
 }
--- src/main/java/com/wora/citronix/repositories/TreeRepository.java
+++ src/main/java/com/wora/citronix/repositories/TreeRepository.java
@@ -1,9 +1,9 @@
 package com.wora.citronix.repositories;
 
-import com.wora.citronix.models.entities.Farm;
+import com.wora.citronix.models.entities.Tree;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FarmRepository extends JpaRepository<Farm, Long>, FarmRepositoryCustom {
+public interface TreeRepository extends JpaRepository<Tree, Long> {
 }
