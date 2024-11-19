a0857a5194b3d9ead543b137f8d12d80436d46ce
hamzalamin <hamzalamin80@gmail.com>
2024-11-19 08:34:55 +0100

    feat: implement save method in TreeService
    - Integrated TreeRepository and TreeMapper to save Tree entities.
    - Implemented save method in TreeService:

--- src/main/java/com/wora/citronix/services/impl/TreeService.java
+++ src/main/java/com/wora/citronix/services/impl/TreeService.java
@@ -1,27 +1,38 @@
 package com.wora.citronix.services.impl;
 
+import com.wora.citronix.mappers.TreeMapper;
 import com.wora.citronix.models.DTOs.treeDtos.CreateTreeDto;
 import com.wora.citronix.models.DTOs.treeDtos.TreeDto;
 import com.wora.citronix.models.DTOs.treeDtos.UpdateTreeDto;
+import com.wora.citronix.models.entities.Tree;
+import com.wora.citronix.repositories.TreeRepository;
 import com.wora.citronix.services.inter.ITreeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TreeService implements ITreeService {
+
+    private final TreeRepository treeRepository;
+    private final TreeMapper treeMapper;
+
     @Override
     public TreeDto save(CreateTreeDto createTreeDto) {
+        Tree tree = treeMapper.toEntity(createTreeDto);
+        Tree savedTree = treeRepository.save(tree);
+        return treeMapper.toDto(savedTree);
+    }
+
+    @Override
+    public TreeDto findById(Long id) {
         return null;
     }
 
     @Override
-    public TreeDto findById(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public TreeDto update(UpdateTreeDto updateTreeDto, Long aLong) {
+    public TreeDto update(UpdateTreeDto updateTreeDto, Long id) {
         return null;
     }
 
@@ -31,7 +42,7 @@ public class TreeService implements ITreeService {
     }
 
     @Override
-    public void delete(Long aLong) {
+    public void delete(Long id) {
 
     }
 }
