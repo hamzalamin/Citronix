80c4b19c08f4f499131f3f9001d2823cb4e0fd82
hamzalamin <hamzalamin80@gmail.com>
2024-11-19 01:15:49 +0100

    feat: add methods for creating and updating Tree entities in TreeMapper
    - Added 	oEntity(CreateTreeDto dto) and 	oEntity(UpdateTreeDto dto) methods for mapping DTOs to Tree entities in the TreeMapper.

--- src/main/java/com/wora/citronix/mappers/TreeMapper.java
+++ src/main/java/com/wora/citronix/mappers/TreeMapper.java
@@ -1,13 +1,17 @@
 package com.wora.citronix.mappers;
 
 import com.wora.citronix.mappers.api.GenericMapper;
+import com.wora.citronix.models.DTOs.treeDtos.CreateTreeDto;
 import com.wora.citronix.models.DTOs.treeDtos.TreeDto;
+import com.wora.citronix.models.DTOs.treeDtos.UpdateTreeDto;
 import com.wora.citronix.models.entities.Tree;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface TreeMapper extends GenericMapper<Tree, TreeDto> {
     Tree toEntity(TreeDto dto);
+    Tree toEntity(CreateTreeDto dto);
+    Tree toEntity(UpdateTreeDto dto);
     TreeDto toDto(Tree tree);
 
 }
