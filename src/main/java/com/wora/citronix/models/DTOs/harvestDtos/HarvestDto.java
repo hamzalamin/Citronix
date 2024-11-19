0df6d2f58c344ff1a5fddc9fdc0dbb2bf369be04
hamzalamin <hamzalamin80@gmail.com>
2024-11-19 15:16:41 +0100

    feat: add HarvestDto with creation date and season
    - Created HarvestDto record to map Harvest entity data.
    - Included fields for id, creationDate, season, and a list of harvestDetails.
    - Integrated validation constraints for required fields.

---
+++ src/main/java/com/wora/citronix/models/DTOs/harvestDtos/HarvestDto.java
@@ -0,0 +1,17 @@
+package com.wora.citronix.models.DTOs.harvestDtos;
+
+import com.wora.citronix.models.DTOs.harvestDetailDtos.EmbeddedHarvestDetailDto;
+import com.wora.citronix.models.enumes.Season;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record HarvestDto(
+        @NotNull @Positive Long id,
+        @NotNull LocalDate creationDate,
+        @NotNull Season season,
+        List<EmbeddedHarvestDetailDto> harvestDetails
+) {
+}
