9f4afad2b6e5028fec63edb1466bb6fa54d46a53
hamzalamin <hamzalamin80@gmail.com>
2024-11-20 00:57:09 +0100

    fix: Correct endpoint spelling and add pagination for finding all harvests
    - Added pagination logic for retrieving all harvests.

--- src/main/java/com/wora/citronix/controllers/HarvestController.java
+++ src/main/java/com/wora/citronix/controllers/HarvestController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1")
@@ -21,6 +20,12 @@ public class HarvestController {
     @PostMapping("/harvests")
     public ResponseEntity<HarvestDto> create(@RequestBody @Valid CreateHarvestDto createHarvestDto){
         return new ResponseEntity<>(harvestService.save(createHarvestDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/harvests")
+    public ResponseEntity<List<HarvestDto>> findAll(@RequestParam int pageNumber, @RequestParam int size){
+        List<HarvestDto> harvests = harvestService.findAll(pageNumber, size);
+        return new ResponseEntity<>(harvests, HttpStatus.OK);
     }
 
 }
