4da9e5e4e29cd4325ca88292d6f3eb49f864abc0
hamzalamin <hamzalamin80@gmail.com>
2024-11-19 15:11:33 +0100

    feat: add creation date and season to Harvest entity
    - Added creationDate and season fields to the Harvest entity.
    - Defined Season enum to represent different seasons (SPRING, SUMMER, AUTUMN, WINTER).

--- src/main/java/com/wora/citronix/models/entities/Harvest.java
+++ src/main/java/com/wora/citronix/models/entities/Harvest.java
@@ -1,12 +1,15 @@
 package com.wora.citronix.models.entities;
 
 
+import com.wora.citronix.models.enumes.Season;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity(name = "harvests")
@@ -18,6 +21,14 @@ public class Harvest {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "creation_date")
+    @NotNull
+    private LocalDate creationDate;
+
+    @Column(name = "season")
+    @NotNull
+    private Season season;
 
     @OneToMany(mappedBy = "harvest", cascade = CascadeType.ALL)
     private List<HarvestDetail> harvestDetails;
---
+++ src/main/java/com/wora/citronix/models/enumes/Season.java
@@ -0,0 +1,5 @@
+package com.wora.citronix.models.enumes;
+
+public enum Season {
+    SPRING,SUMMER,AUTUMN,WINTER
+}
