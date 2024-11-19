aa0e0b11c5e223aedf92f1afedb80a955905d66c
hamzalamin <hamzalamin80@gmail.com>
2024-11-19 13:39:48 +0100

    test: add test for save method to handle field not found exception
    - Added a test case to verify that the save method in TreeService throws an EntityNotFoundException when the field ID is not found.

--- src/test/java/com/wora/citronix/services/impl/TreeServiceTest.java
+++ src/test/java/com/wora/citronix/services/impl/TreeServiceTest.java
@@ -1,20 +1,33 @@
 package com.wora.citronix.services.impl;
 
+import com.wora.citronix.exceptions.EntityNotFoundException;
 import com.wora.citronix.mappers.TreeMapper;
 import com.wora.citronix.models.DTOs.treeDtos.CreateTreeDto;
+import com.wora.citronix.repositories.TreeRepository;
 import com.wora.citronix.services.inter.IFieldService;
 import com.wora.citronix.services.inter.ITreeService;
 import net.bytebuddy.implementation.bytecode.Throw;
+import org.assertj.core.api.Assertions;
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
+@ExtendWith(SpringExtension.class)
 class TreeServiceTest {
 
+    @Mock
+    private TreeRepository treeRepository;
     @Mock
     private TreeMapper treeMapper;
     @Mock
@@ -28,5 +41,18 @@ class TreeServiceTest {
         CreateTreeDto createTreeDto = null;
         assertThrows(NullPointerException.class , () -> sut.save(createTreeDto));
 
+    }
+
+    @Test
+    @DisplayName("save() Should throw Exception When Field Not found")
+    void save_ShouldThrowExceptionWhenFieldNotFound(){
+        LocalDate plantingDate = LocalDate.parse("2020-06-06");
+        Long fieldId = 9999L;
+
+        CreateTreeDto createTreeDto = new CreateTreeDto(plantingDate, fieldId);
+        given(fieldService.getFieldEntityById(fieldId)).willThrow(new EntityNotFoundException("field", fieldId));
+        assertThatExceptionOfType(EntityNotFoundException.class)
+                .isThrownBy(() -> sut.save(createTreeDto))
+                .withMessage("field with the id " + fieldId +" not found");
     }
 }
