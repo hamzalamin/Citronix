fab64c12bff56abbbeb08887d9b4f862e61a1127
hamzalamin <hamzalamin80@gmail.com>
2024-11-20 14:07:16 +0100

    feat: Add DTOs for HarvestDetail and EmbeddedHarvestDetail
    - Created HarvestDetailsDto for returning detailed harvest information with validation on quantity.
    - Created EmbeddedHarvestDetailDto for embedding harvest details with validation on quantity.

--- src/main/java/com/wora/citronix/models/DTOs/harvestDetailDtos/EmbeddedHarvestDetailDto.java
+++ src/main/java/com/wora/citronix/models/DTOs/harvestDetailDtos/EmbeddedHarvestDetailDto.java
@@ -1,8 +1,11 @@
 package com.wora.citronix.models.DTOs.harvestDetailDtos;
 
 import com.wora.citronix.models.entities.embeddables.HarvestDetailsId;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record EmbeddedHarvestDetailDto(
-        HarvestDetailsId id
+        HarvestDetailsId id,
+        @Positive @NotNull Integer quantity
 ) {
 }
