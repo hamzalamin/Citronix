f051cde06bc7a24d9507aaaf821e374a59e294ea
hamzalamin <hamzalamin80@gmail.com>
2024-11-20 14:04:41 +0100

    feat: Add quantity field with validation to HarvestDetail entity

--- src/main/java/com/wora/citronix/models/entities/HarvestDetail.java
+++ src/main/java/com/wora/citronix/models/entities/HarvestDetail.java
@@ -2,6 +2,8 @@ package com.wora.citronix.models.entities;
 
 import com.wora.citronix.models.entities.embeddables.HarvestDetailsId;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,10 @@ import lombok.Setter;
 public class HarvestDetail {
     @EmbeddedId
     private HarvestDetailsId id;
+
+    @NotNull
+    @Positive
+    private Integer quantity;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "harvest_id", insertable = false, updatable = false)
