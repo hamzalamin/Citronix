1ec399c94309c38c77c16373d014518bdb3c8a05
hamzalamin <hamzalamin80@gmail.com>
2024-11-20 01:08:13 +0100

    feat: Implement update endpoint for harvest
    - Added PUT endpoint to update a harvest with validation and proper response.
    - Status code changed to HttpStatus.OK to reflect successful update.

--- src/main/java/com/wora/citronix/controllers/HarvestController.java
+++ src/main/java/com/wora/citronix/controllers/HarvestController.java
@@ -2,6 +2,7 @@ package com.wora.citronix.controllers;
 
 import com.wora.citronix.models.DTOs.harvestDtos.CreateHarvestDto;
 import com.wora.citronix.models.DTOs.harvestDtos.HarvestDto;
+import com.wora.citronix.models.DTOs.harvestDtos.UpdateHarvestDto;
 import com.wora.citronix.services.inter.IHarvestService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -31,6 +32,11 @@ public class HarvestController {
     @GetMapping("/harvest/{id}")
     public ResponseEntity<HarvestDto> findById(@PathVariable Long id){
         return new ResponseEntity<>(harvestService.findById(id), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/harvest/{id}")
+    public ResponseEntity<HarvestDto> update(@RequestBody @Valid UpdateHarvestDto updateHarvestDto, @PathVariable Long id){
+        return new ResponseEntity<>(harvestService.update(updateHarvestDto, id), HttpStatus.CREATED);
     }
 
 
