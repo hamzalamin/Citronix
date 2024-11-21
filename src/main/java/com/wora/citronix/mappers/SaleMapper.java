e4a9d5800421094ed6fad466c64a8e1b1f28258a
hamzalamin <hamzalamin80@gmail.com>
2024-11-21 14:01:56 +0100

    feat: Enhance SaleMapper with additional DTO conversions
    - Added methods to SaleMapper for converting CreateSaleDto and UpdateSaleDto to Sale entity.
    - Ensured SaleMapper supports bidirectional conversion between SaleDto and Sale entity.

--- src/main/java/com/wora/citronix/mappers/SaleMapper.java
+++ src/main/java/com/wora/citronix/mappers/SaleMapper.java
@@ -1,8 +1,16 @@
 package com.wora.citronix.mappers;
 
 import com.wora.citronix.mappers.api.GenericMapper;
+import com.wora.citronix.models.DTOs.saleDtos.CreateSaleDto;
 import com.wora.citronix.models.DTOs.saleDtos.SaleDto;
+import com.wora.citronix.models.DTOs.saleDtos.UpdateSaleDto;
 import com.wora.citronix.models.entities.Sale;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface SaleMapper extends GenericMapper<Sale, SaleDto> {
+    Sale toEntity(SaleDto saleDto);
+    Sale toEntity(CreateSaleDto createSaleDto);
+    Sale toEntity(UpdateSaleDto updateSaleDto);
+    SaleDto toDto(Sale sale);
 }
