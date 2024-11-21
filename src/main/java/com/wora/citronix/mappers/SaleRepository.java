f51a76ea26326e54c974eca8d5b3f436a0a1b54b
hamzalamin <hamzalamin80@gmail.com>
2024-11-21 13:42:16 +0100

    add: Create SaleRepository for Sale entity
    - Implemented SaleRepository interface extending JpaRepository.
    - Enables CRUD operations for the Sale entity.

---
+++ src/main/java/com/wora/citronix/mappers/SaleRepository.java
@@ -0,0 +1,7 @@
+package com.wora.citronix.mappers;
+
+import com.wora.citronix.models.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+}
