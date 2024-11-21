8bc15fe8cec9cba7adaca429041bfb8561d3304b
hamzalamin <hamzalamin80@gmail.com>
2024-11-21 13:44:35 +0100

    refactor: Rename SaleRepository to SaleMapper
    - Replaced SaleRepository with SaleMapper implementing GenericMapper.
    - Aligned the codebase with the generic mapping strategy.

---
+++ src/main/java/com/wora/citronix/mappers/SaleMapper.java
@@ -0,0 +1,7 @@
+package com.wora.citronix.mappers;
+
+import com.wora.citronix.mappers.api.GenericMapper;
+import com.wora.citronix.models.entities.Sale;
+
+public interface SaleMapper extends GenericMapper<Sale, SaleDto> {
+}
--- src/main/java/com/wora/citronix/mappers/SaleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wora.citronix.mappers;
-
-import com.wora.citronix.models.entities.Sale;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SaleRepository extends JpaRepository<Sale, Long> {
-}
