f6b06e098fa91fcf03fc537475c2d82dda21910e
hamzalamin <hamzalamin80@gmail.com>
2024-11-22 08:50:28 +0100

    test: Add unit test for SaleService save() method with quantity validation
    - Tests that save() throws PlantingDateException when the wanted quantity exceeds the available harvested quantity.
    - Mocks IHarvestService to return a valid harvest entity.
    - Mocks ISaleQuantityValidatorService to simulate the exception for exceeding quantity.
    - Validates exception handling and error messaging in the service method.

--- src/test/java/com/wora/citronix/services/impl/SaleServiceTest.java
+++ src/test/java/com/wora/citronix/services/impl/SaleServiceTest.java
@@ -1,6 +1,7 @@
 package com.wora.citronix.services.impl;
 
 import com.wora.citronix.exceptions.EntityNotFoundException;
+import com.wora.citronix.exceptions.PlantingDateException;
 import com.wora.citronix.mappers.SaleMapper;
 import com.wora.citronix.models.DTOs.saleDtos.CreateSaleDto;
 import com.wora.citronix.models.entities.Harvest;
@@ -19,7 +20,9 @@ import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.in;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -51,4 +54,25 @@ class SaleServiceTest {
                 .isThrownBy(() -> sut.save(createSaleDto))
                 .withMessage("Harvest with the id " + harvest.getId() + " not found");
     }
+
+    @Test
+    @DisplayName("save Should Throw Planting Date Exception When the Quantity Wanted mora then Quantity Harvested")
+    void save(){
+        LocalDate saleDate = LocalDate.parse("2024-07-07");
+        LocalDate creationDate = LocalDate.parse("2024-01-01");
+
+        Harvest harvest = new Harvest(1L ,creationDate, Season.WINTER, List.of(), List.of());
+        CreateSaleDto createSaleDto = new  CreateSaleDto("HAMZA", 12.2, saleDate, 12.2, harvest.getId());
+
+        given(harvestService.findEntityById(harvest.getId())).willReturn(harvest);
+        doThrow(new PlantingDateException("the wanted quantity does not exist"))
+                .when(iSaleQuantityValidatorService)
+                .ensureWantedQuantityExist(12.2, harvest.getId());
+
+        assertThatExceptionOfType(PlantingDateException.class)
+                .isThrownBy(() -> sut.save(createSaleDto))
+                .withMessage("the wanted quantity does not exist");
+
+    }
+
 }
