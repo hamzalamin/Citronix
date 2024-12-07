653a20d275359d7d2109d4682b4f9b64d406dd2e
hamzalamin <hamzalamin80@gmail.com>
2024-12-07 15:04:14 +0100

    Add CORS configuration to allow React app on localhost:3000 to access API

--- src/main/java/com/wora/citronix/configs/OpenApiConfig.java
+++ src/main/java/com/wora/citronix/configs/OpenApiConfig.java
@@ -7,6 +7,8 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -33,5 +35,19 @@ public class OpenApiConfig {
                         "With Citronix, farmers can enhance operational efficiency and gain insights into their farm's performance to make informed decisions.")
                 .contact(contact);
         return new OpenAPI().info(info).servers(List.of(server));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
